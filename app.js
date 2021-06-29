22eb517c20de1a21eb54e56bf14fa904e67b58fc
brickrend <Brickrends9@gmail.com>
2021-06-29 19:05:30 +0300

    delete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // express
 const express = require("express");
 // data
-const products = require("./data");
+let products = require("./data");
 //cors
 const cors = require("cors");
 
@@ -13,6 +13,20 @@ app.get("/product", (req, res) => {
   res.json(products);
 });
 
+app.delete("/product/:productId", (req, res) => {
+  const productId = req.params.productId;
+  console.log(productId);
+  const foundProduct = products.find((product) => product.id === +productId);
+
+  if (foundProduct) {
+    products = products.filter((product) => product.id !== +productId);
+    console.log(products);
+    res.json(products).status(204).end;
+  } else {
+    res.status(404).json({ message: "not found" });
+  }
+});
+
 app.listen(8000, () => {
-  console.log("workding");
+  console.log("working");
 });
